fc921e9064fab87a60d829da672734638285b4f2
Piero-HO <piero.huaytalla@tecsup.edu.pe>
2026-06-10 17:27:46 -0500

    uso de shadcn

--- drangonball/src/App.tsx
+++ drangonball/src/App.tsx
@@ -1,9 +1,8 @@
-
+import { Button } from '@/components/ui/button'
 
 function App() {
-  return (<div className="App">
-      <h1>Dragon Ball</h1>
-    </div>
+  return (
+    <Button>Dragon Ball</Button>
   );
 }
 
